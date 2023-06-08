c40d377df0aedd55a1bb1bd7a6f6f861a24b1c86
Ben Lamb <benlamb@vt.edu>
2023-06-08 12:59:34 -0400

    Added GUI frontend with styling

--- src/main/java/net/lamb/JavaWebApp/JavaWebAppApplication.java
+++ src/main/java/net/lamb/JavaWebApp/JavaWebAppApplication.java
@@ -2,12 +2,30 @@ package net.lamb.JavaWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @SpringBootApplication
+@Controller
 public class JavaWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebAppApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaWebAppApplication.class, args);
-	}
+    @GetMapping("/")
+    public String home(@ModelAttribute("message") String message, Model model) {
+        model.addAttribute("message", message);
+        return "home";
+    }
 
+    @PostMapping("/")
+    public String postMessage(@RequestParam("message") String message, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", message);
+        return "redirect:/";
+    }
 }
